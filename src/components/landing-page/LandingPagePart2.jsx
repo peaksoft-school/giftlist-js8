3c71b52cb82d08dcb5750579dcce35e24a94a676
FrontenderO07 <nurlanbekovich2006@gmail.com>
2023-04-14 10:54:17 +0600

    fixed

---
+++ src/components/landing-page/LandingPagePart2.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ThrirdSection from './ThrirdSection'
+import FourSection from './FourSection'
+import FiveSection from './FiveSection'
+import Footer from './Footer'
+
+const LandingPagePart2 = () => {
+  return (
+    <>
+    <ThrirdSection/>
+    <FourSection/>
+    <FiveSection/>
+    <Footer/>
+    </>
+  )
+}
+
+export default LandingPagePart2
